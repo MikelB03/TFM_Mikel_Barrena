505f0ebbb63a842b88bdff101ae7f9552c0d57ae
mbarrena <mbarrena@ikerlan.es>
2025-02-07 10:12:54 +0100

    ra4m1_fromscilab program

---
+++ src/ICU_config.h
@@ -0,0 +1,28 @@
+/*
+ * ICU_config.h
+ *
+ *  Created on: 15 ene 2025
+ *      Author: mbarrena
+ */
+
+#include "hal_data.h"
+#include "r_icu.h"
+#include "r_external_irq_api.h"
+#include "common_data.h"
+
+#ifndef ICU_CONFIG_H_
+#define ICU_CONFIG_H_
+
+/* External IRQ channel for BOARD_RA4M1_EK*/
+#define USER_SW_IRQ_NUMBER        (0x00)
+
+extern volatile bool g_sw_S1_press;
+
+
+/* Function declaration */
+fsp_err_t icu_init(void);
+fsp_err_t icu_enable(void);
+void icu_deinit(void);
+void Parpadeo_LED (void);
+
+#endif /* ICU_CONFIG_H_ */
